8a61a9c632e80a471b2be5b3e088007d09e34821
Bryce Monaco <Bryce@BrycBook-Pro.local>
2018-10-23 19:19:11 -0700

    Began work on Training.cpp

--- Project2/Training.cpp
+++ Project2/Training.cpp
@@ -1,4 +1,98 @@
 /**
  * This program reads in strings from a CSV file
- * 
+ * Build with: g++ Training.cpp -o training -std=c++11
  */
+
+ #include <iostream>
+ #include <fstream>
+ #include <string>
+ #include <vector>
+ #include <map>
+
+ using namespace std;
+
+int main (int argc, char* argv [])
+{
+    ifstream trainingDataFile;
+    ofstream outputFile;
+
+    map <string, int>* spamMap = new map <string, int> ();
+    map <string, int>* hamMap = new map <string, int> ();
+
+
+    //Dealloc Operations
+    delete spamMap;
+    delete hamMap;
+
+}
+
+/**
+ * Recieves a line from main (), and handles calling all of the various functions to decode
+ * Returns true if the line was successfully decoded, false otherwise.
+ */
+bool DecodeLine (string line)
+{
+    int spamOrHam = GetSpamOrHam (line);
+
+    if (spamOrHam == 0) //Ham
+    {
+        line.erase (0, 4);
+
+    } else if (spamOrHam == 1) //Spam
+    {
+        line.erase (0, 5);
+
+    } else
+    {
+        cout << "Unrecognized ham or spam \"" << line << "\"" << endl;
+
+        return false;
+
+    }
+
+}
+
+/**
+ * Used to parse in the first CSV value, which should be either "ham" or "spam".
+ * Returns 0 if ham, 1 if spam, -1 otherwise (unknown)
+ */
+int GetSpamOrHam (string line)
+{
+    if (line.substr (0, 3) == "ham")
+    {
+        return 0;
+
+    } else if (line.substr (0, 4) == "spam")
+    {
+        return 1;
+
+    } else
+    {
+        return -1;
+
+    }
+}
+
+string ConvertToAlphaNumeric (string sentString)
+{
+    for (int i = 0; i < sentString.length(); i++)
+    {
+        if (!IsCharAlphaNumeric (sentString [i]))
+        {
+            sentString.erase (sentString.begin() + i);
+
+        }
+
+    }
+
+}
+
+/**
+ * Used to check if a char is within the ranges [0, 9], [a, z], or [A, Z] 
+ */
+bool IsCharAlphaNumeric (char sentChar)
+{
+    return (sentChar >= '0' && sentChar <= '9') || (sentChar >= 'a' && sentChar <= 'z') || (sentChar >= 'A' && sentChar <= 'Z');
+
+}
+
--- README.md
+++ README.md
@@ -4,5 +4,6 @@ This is a repository for all of my homework and project code for CS 482.
 ### Project 1: Tic Tac Toe
 This project consists of using a provided API to create a program which can win or tie any game of tic tac toe.
 
-
+### Project 2: Spam Filter
+This project consists of two programs. The first, Training.cpp takes in a set of training data, classifies words as spam or ham, and then sends that classification to another program (Classify) which will scan through a different set of data and classify messages in that different data as spam or ham based on the classification of the first program.
 
