0bf6a01e9261a1f3a7ad595e8da9e8a57b9d279c
Bryce Monaco <brycedmonaco@gmail.com>
2018-11-13 22:14:58 -0800

    Implemented kf_update

--- Project3/kf.py
+++ Project3/kf.py
@@ -1,5 +1,5 @@
 # kf.py updated file for robot estimation assignment
-# pset[3]
+# project[2]
 # (C) 2017 David Feil-Seifer
 # Modified by Bryce Monaco, Fall 2018
 
@@ -12,12 +12,34 @@ import scipy.stats
 # kf_update: update state estimate [u, sigma] with new control [xdot] and measurement [z]
 # 	parameters:
 #			u : 2x1 vector with state estimate (x) at time t-1 and control (xdot) at time t
+#				u[0]=state estimate (x_t-1)
+#				u[1]=velocity (xdot)
 #			sigma: 2x2 matrix with covariance at time t-1
 #			z (int): observed (uncertain) measurement of state (x) at time t
 #	returns: [u sigma] updated state with estimate at time t
 
 def kf_update(u, sigma, z):
-	return [u, sigma]
+	# Variable setup
+	delta_t = 0.1  # Need to find this value
+	little_sigma_x = 100
+	little_sigma_z = 100
+	
+	u_now = u
+
+	big_h = np.matrix([1, 0])
+	big_g = np.matrix([delta_t * delta_t, delta_t / 2]).T  # G = [delta_t^2; delta_t/2]
+
+	big_sigma_x = np.linalg.matrix_power(big_g * big_g.T, little_sigma_x)
+	big_sigma_z = little_sigma_z * little_sigma_z
+	big_sigma_now = sigma
+
+	big_f = np.matrix([[1, delta_t], [0, 1]])
+
+	big_k_next = ((big_f * big_sigma_now * big_f.T) + big_sigma_x) * big_h.T * np.linalg.inv(big_h * ((big_f * big_sigma_now * big_f.T) + big_sigma_x) * big_h.T + big_sigma_z)
+	big_sigma_next = (np.identity(2) - (big_k_next * big_h)) * ((big_f * big_sigma_now * big_f.T) + big_sigma_x)
+	u_next = big_f * u_now + big_k_next * (z - big_h * big_f * u_now)  # Note: unsure if z or z_t+1. Book says z_t+1 but idk how to get that
+
+	return [u_next, big_sigma_next]
 
 
 # door_update: update estimate of door locations
