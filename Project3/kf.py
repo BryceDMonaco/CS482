45c10c396b2ccbbbf4b76712c6f48f4854439ea0
Bryce Monaco <brycedmonaco@gmail.com>
2018-11-15 16:59:08 -0800

    Implemented door_update

--- Project3/kf.py
+++ Project3/kf.py
@@ -24,7 +24,7 @@ def kf_update(u, sigma, z):
 	little_sigma_x = 5
 	little_sigma_z = 10
 	
-	u_now = u
+	u_now = u  # Technically not needed, but helps keep everything in the same format
 
 	big_h = np.matrix([1, 0])
 	big_g = np.matrix([delta_t * delta_t, delta_t / 2]).T  # G = [delta_t^2; delta_t/2]
@@ -50,15 +50,28 @@ def kf_update(u, sigma, z):
 #			door_dist (array of size 10): probability (0..1) that a door exists at each location (0..9)
 #	returns: [door_dist] updated door distribution
 
+count_array = [[1,2],[1,2],[1,2],[1,2],[1,2],[1,2],[1,2],[1,2],[1,2],[1,2]]  # 2D array of counts for each door, [#counts there is a door, #counts there is not a door]
+
 def door_update(u, sigma, d, door_dist):
-	door_index = int(u[0]/1000)  # Used so we know which door's probability to change
+	door_index = int(u[0]/100)  # Used so we know which door's probability to change
 
 	"""
 	D = There is a door
 	S = The sensor is correct
+	P(S)=0.6*0.8=0.48		The door sensor is correct, 48% of the time overall
+	P(S|D)=0.6				The door sensor says true when there is a door 60% of the time
 	P(D|S)=P(S|D)*P(D)/P(S)
-	P(D|S)=0.6*(1/1024)/0.48=0.00122070312
+	P(D|S)=0.6*P(D)/0.48=1.25*P(D)=1.25*(#times d/#times !d)
 	"""
+
+	if d is True:
+		count_array[door_index][0] = count_array[door_index][0] + 1
+	else:
+		count_array[door_index][1] = count_array[door_index][1] + 1
+
+	prob_d = count_array[door_index][0] / count_array[door_index][1]
+
+	door_dist[door_index] = np.clip(1.25*prob_d, 0, 1)  # This shouldn't need to be clipped (clamped) but floating point error sometimes makes it 1.00000001 which makes the red bar fill the entire vertical space above it
 
 	return door_dist
 
